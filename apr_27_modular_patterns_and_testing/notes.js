c80ab0425316c82480f7dcf07ea8f94ac29124dc
Jonathan Ullberg <jonathan.ullberg@gmail.com>
2015-04-27 15:35:50 -0700

    added greet.js files

---
+++ apr_27_modular_patterns_and_testing/notes.js
@@ -0,0 +1,8 @@
+//	Using Require and exports more than any other methods
+//	Everything in node is an IIFE
+//	Node Global	- What's in the global scope and what isn't?
+	//	require()
+	//	exports
+	//	__dirname
+	//	process
+	//	console
