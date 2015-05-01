8c4811f38aaddc8c432968e979b9381b1a2995f4
Jonathan Ullberg <jonathan.ullberg@gmail.com>
2015-05-01 08:49:46 -0700

    updated all apr_27 files and .gitignore

--- apr_27_modular_patterns_and_testing/modular_and_testing/greet_2.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-exports.greet = function(name) {
-	return 'hello ' + name;
-};
--- apr_27_modular_patterns_and_testing/modular_and_testing/greet_3.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var Greet = module.exports = exports = function() {};
-
-Greet.prototype.greet = function(name) {
-	return 'hello ' + name;
-}
